5271dec930579830014a6138cb6061d4f0088da3
derektan12 <derektan101@gmail.com>
2024-05-17 11:32:02 -0400

    Android Camera

---
+++ app/src/main/java/com/example/ioc/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.ioc
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Space
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCamera: Button
+    private lateinit var imageView: ImageView
+
+    companion object { // Creating a static variable in Kotlin
+        private const val CAMERA_REQUEST_CODE = 100
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnCamera = findViewById(R.id.btncamera_id)
+        imageView = findViewById(R.id.imageview1)
+
+        btnCamera.setOnClickListener {
+            openCamera()
+        }
+
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Camera not available.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
+        }
+        else {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
